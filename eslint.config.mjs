84476a316faa550af8c7c8d57b24e94c563b8243
Ashenafi Damena <ashenafidamena415@gmail.com>
2025-10-10 13:14:40 +0300

    Eslint feature added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,15 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfigs: js.configs.all,
 });
 
 const eslintConfig = [
